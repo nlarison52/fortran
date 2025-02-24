a897aae8e2b875256f36e4fbc5707ae52805dc47
Nolan Larison <larison.15@buckeyemail.osu.edu>
2025-02-24 16:37:45 -0500

    further developed rref

--- matrix_operations/math.f90
+++ matrix_operations/math.f90
@@ -2,7 +2,7 @@ module math
     implicit none
 
 contains
-    function mm(x, y) result(c)
+    function mm(x, y) result(c) ! basic matmul implementation
         integer, allocatable, dimension(:, :) :: x, y, c
         integer, dimension(2) :: dimx, dimy
         integer :: i, j, k
@@ -27,38 +27,80 @@ contains
     end function mm
 
 
-    function rref(X, b) result(Z)
-        real, dimension(:, :) :: X, b
+    function rref(X) result(Z) 
+        real, dimension(:, :) :: X
         real, allocatable, dimension(:, :) :: Z
-        integer, dimension(2) :: dimx, dimb, dimz
+        integer, dimension(2) :: dimx
         integer :: i, j
+        real, parameter :: eps = 1.0E-7
 
         dimx = shape(X)
-        dimb = shape(b)
-        
-        if (dimx(1) /= dimx(2)) then
-            error stop "X must be a square matrix"
-        end if
 
-        if ((dimb(1) /= dimx(1)) .or. (dimb(2) /= 1)) then
-            error stop "b must be a vecotor with Nx1 where N is dim(X)"
-        end if
+        do i = 1, dimx(1)
+            if (abs(X(i, i)) < eps) then
+                do j = i + 1, dimx(1)
+                    if (X(j, i) /= 0) then
+                       call swap_rows(X, i, j)
+                       exit
+                   end if 
+                end do
+            end if
 
-        allocate(Z(dimx(1), dimx(2)+1))
-
-        dimz = shape(Z)
-        Z(:, 1:dimx(2)) = X
-        Z(:, dimx(2)+1) = b
-
-        do i = 1, dimz(1) !this works because base matrix is square
-            call normalize(Z, i)
-            call clear_col(Z, i)
-        
             
+
         end do
 
 
+
+
+        
     end function rref
+
+
+    subroutine normalize_row(X, row, col)
+        implicit none
+        real, intent(inout), dimension(:,:) :: X
+        integer, intent(in) :: row, col
+        real, parameter :: eps = 1.0E-7  ! Small threshold for floating-point safety
+
+        ! Check if X(row, col) is "effectively zero" before dividing
+        if (abs(X(row, col)) > eps) then
+            X(row, :) = X(row, :) / X(row, col)  ! ✅ Safe division
+        else
+            error stop "Pivot element is too close to zero for normalization!"  
+        end if
+    end subroutine normalize_row
+
+
+    subroutine clear_col(X, row, col)
+        implicit none
+        real, intent(inout), dimension(:, :) :: X
+        integer, intent(in) :: row, col
+        integer :: i
+
+        do i = 1, shape(X, 1)
+            if (i == row) cycle
+            X(i, :) = X(i, :) - X(row, :) * X(i, col)
+        end do
+
+    end subroutine clear_col
+
+
+    subroutine swap_rows(X, i, j)
+        real, intent(inout), dimension(:, :) :: X
+        integer :: i, j 
+        real, allocatable, dimension(:) :: temp
+        integer, dimension(2) :: dimx
+
+        dimx = shape(X)
+
+        allocate(temp(dimx(2)))
+
+        temp = X(i, :)
+        X(i, :) = X(j, :)
+        X(j, :) = temp
+
+    end subroutine swap_rows
 
 
     function is_reduced(Z) result(res)
