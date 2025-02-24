84fd3d5046939aebbabfc11207be532b5bdac9d8
Nolan Larison <larison.15@buckeyemail.osu.edu>
2025-02-23 21:31:48 -0500

    dynamic memory and array operations practice

--- main.f90
+++ main.f90
@@ -1,20 +1,22 @@
 program main
     implicit none
-    integer :: x, y
-    x = 6
-    print *, factorial(x)
+    real :: x, y
+
+    print *, "Enter a temperature in F: "
+    read *, x
+
+    y = convert(x)
+
+    print *, "Temperature in C: ", y
 
 contains
-    recursive function factorial(x) result(z)
-        integer, intent(in):: x
-        integer :: z
 
-        if (x == 1) then 
-            z = 1
-        else
-            z = x * factorial(x - 1)
-        end if
-    end function factorial
+     function convert(f) result(c)
+        real, intent(in) :: f
+        real :: c
+
+        c = 5.0 / 9.0 * (f - 32.0)
+    end function convert
 
 
 end program main
