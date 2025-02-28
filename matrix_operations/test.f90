83c980341e90fd6563ff0e44d408943f0acdb120
Nolan Larison <larison.15@buckeyemail.osu.edu>
2025-02-28 11:42:18 -0500

    tried rref with lapack to verify operation

--- matrix_operations/test.f90
+++ matrix_operations/test.f90
@@ -2,33 +2,61 @@ program test_rref
     use math
     implicit none
 
-    real, allocatable, dimension(:,:) :: A, R
-    integer :: i, j
+    real, allocatable, dimension(:,:) :: A, R, LU
+    integer, allocatable, dimension(:) :: ipiv
+    integer :: i, j, n, m, info
 
-    ! allocate and define a test matrix
-    allocate(A(3,4))  ! example 3x4 augmented matrix
+    ! define matrix size
+    n = 3
+    m = 4  ! augmented system
 
-    ! defining the test matrix
-    A = reshape([1.0, 2.0, 3.0, &
-             4.0, 5.0, 6.0, &
-             0.0, 0.0, 0.0], [3,3]) 
+    ! allocate matrices
+    allocate(A(n, m), R(n, m), LU(n, m))
+    allocate(ipiv(n))
 
-!     print original matrix
+    ! defining test matrix
+    A = reshape([1.0, 2.0, 3.0, 8.0, &
+                 4.0, 5.0, 6.0, 3.0, &
+                 0.0, 0.0, 0.0, 0.0], [n,m])
+
+    ! print original matrix
     print *, "original matrix A:"
-    do i = 1, size(A,1)
+    do i = 1, n
         print "(4F8.3)", A(i, :)
     end do
 
-    ! compute rref
+    ! compute rref using our function
     R = rref(A)
 
-    ! print reduced matrix
-    print *, "reduced row echelon form (rref) of A:"
-    do i = 1, size(R,1)
+    ! print our rref result
+    print *, "rref(A) result:"
+    do i = 1, n
         print "(4F8.3)", R(i, :)
     end do
 
+    ! copy A into LU before using LAPACK
+    LU = A
+
+    ! compute LU decomposition (row echelon form)
+    call dgetrf(n, m, LU, n, ipiv, info)
+
+    ! print LU decomposition (row echelon form)
+    print *, "LU decomposition (row echelon form):"
+    do i = 1, n
+        print "(4F8.3)", LU(i, :)
+    end do
+
+    ! compare LU with RREF
+    print *, "comparison of rref(A) vs LU:"
+    do i = 1, n
+        do j = 1, m
+            if (abs(R(i, j) - LU(i, j)) > 1.0E-6) then
+                print *, "mismatch at (", i, ",", j, "): ", R(i, j), " vs ", LU(i, j)
+            end if
+        end do
+    end do
+
     ! cleanup
-    deallocate(A, R)
+    deallocate(A, R, LU, ipiv)
 
 end program test_rref
