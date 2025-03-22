29017350d2552d07456da610505d7dfc26edb374
Nolan Larison <larison.15@buckeyemail.osu.edu>
2025-03-21 22:17:08 -0400

    experimented with parallel compute matmul
    very slow version that just runs dot products per entry

--- coarr/test.f90
+++ coarr/test.f90
@@ -1,19 +1,39 @@
 program test
-    implicit none
-    integer :: my_rank
-    integer, allocatable, dimension(:) :: x[:]
+  implicit none
+  integer :: my_rank, row, col, i, j, source_image
+  real(8) :: x(2, 2), z(2, 2)[*]
+  integer :: num_procs
 
-    my_rank = this_image()
+  ! Explicit declaration
+  real(8), external :: ddot
 
-    allocate(x(my_rank)[*])
+  x = 1.0d0
+  my_rank = this_image()
+  num_procs = num_images()
 
-    x = my_rank
+  if (num_procs /= 4) then
+      if (my_rank == 1) print *, "Run with exactly 4 images!"
+      stop
+  end if
 
-    sync all
+  ! Correct mapping of image rank to matrix indices
+  row = (my_rank - 1) / 2 + 1
+  col = mod(my_rank - 1, 2) + 1
 
-    print *, "Im num: ", my_rank
-    print *, "Value: ", x
+  ! Compute the matrix element (row, col)
+  z(row, col) = ddot(2, x(row, :), 2, x(:, col), 1)
 
+  sync all  ! Wait for all computations
 
+  if (my_rank == 1) then
+      print *, "Final Matrix is:"
+      do i = 1, 2
+          do j = 1, 2
+              source_image = (i - 1) * 2 + j
+              z(i,j) = z(i,j)[source_image]
+          end do
+          print '(2F8.2)', z(i,:)
+      end do
+  end if
 
 end program test
