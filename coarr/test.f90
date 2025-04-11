af26b27a76158a38ad2e6cf5fc1886cbd5234c53
Nolan Larison <larison.15@buckeyemail.osu.edu>
2025-04-11 18:43:12 -0400

    started wroking on parallel axpy

--- coarr/test.f90
+++ coarr/test.f90
@@ -1,5 +1,41 @@
 program test
+    integer, parameter :: n = 10
+    real :: x(n), y(n), z(n)
+    real :: a
+
+    call random_seed()
+    call random_number(a)
+    call random_number(y)
+    call random_number(x)
+
+    a = a * 100 + 1
+    y = y * 100 + 1
+    x = x * 100 + 1
+
+    call axpy(a, x, y, n, z)
+
+    print *, z
+    
+
+
+    
 
 
 
+
+contains
+
+    subroutine axpy(a, x, y, n, z)
+        ! n is dim x
+        implicit none
+        integer :: i
+        integer, intent(in) :: n
+        real, intent(in) :: a, x(:), y(:)
+        real, intent(inout) :: z(:)
+
+        do i = 1, n
+            z(i) = x(i) * a + y(i)
+        end do 
+
+    end subroutine axpy
 end program test
